68e6a78e827cf11d16566021eac5161306acdff3
Nikita Bulakhov <bulahov26@gmail.com>
2019-12-13 21:45:56 +0300

    hotfix to age user

--- app/Model/User.php
+++ app/Model/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable implements JWTSubject
     protected $fillable = [
         'name',
         'email',
+        'age',
         'password',
         'phone',
         'surname',
